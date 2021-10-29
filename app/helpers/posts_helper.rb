53efc192bb517fb709f61b4c8543772a371ba78f
Merinda Lima <melrylimace@gmail.com>
2021-10-28 23:34:25 -0300

    Ajuste para Imagem de perfil de usuário e fundo de capa

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+
+    def get_image_user(profile, position)
+        img = ''
+        if position == 'img_user'
+            img = profile.image.present? ? profile.image : '/assets/img/user2-160x160.png'
+        elsif position == 'front_user'    
+            img = profile.front_cover.present? ? profile.front_cover : '/assets/img/user2-160x160.png'
+        end    
+        img
+    end      
 end
