6e10bbe2a467d0adb27eea44cbfe8d714f959953
Merinda Lima <melrylimace@gmail.com>
2021-10-29 06:01:03 -0300

    Adjustes em Posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,9 +3,9 @@ module PostsHelper
     def get_image_user(profile, position)
         img = ''
         if position == 'img_user'
-            img = profile.image.present? ? profile.image : '/assets/img/user2-160x160.png'
+            img = profile.image rescue '/assets/img/user2-160x160.png'
         elsif position == 'front_user'    
-            img = profile.front_cover.present? ? profile.front_cover : '/assets/img/user2-160x160.png'
+            img = profile.front_cover rescue '/assets/img/user2-160x160.png'
         end    
         img
     end      
